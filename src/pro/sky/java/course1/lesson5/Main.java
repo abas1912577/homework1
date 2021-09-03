c56cb8765466490cffff86090c2fa26143f7b628
abas1912577 <abas1912577@yandex.ru>
2021-09-03 11:31:54 +0300

    Homework1 is done

---
+++ src/pro/sky/java/course1/lesson5/Main.java
@@ -0,0 +1,72 @@
+package pro.sky.java.course1.lesson5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Сумма затрат за месяц составил " + sum + " рублей.");
+
+        int maxCost = 0;
+        int minCost = 200000;
+        for (int a = 0; a != arr.length; a++) {
+            if (arr[a] > maxCost) {
+                maxCost = arr[a];
+            }
+            if (arr[a] < minCost) {
+                minCost = arr[a];
+            }
+        }
+        System.out.println("Максимальная сумма затрат за месяц составила " + maxCost + " рублей. Минимальная сумма затрат за месяц составила " + minCost + " рублей.");
+
+        double midCost = sum / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + midCost + " рублей.");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.println(reverseFullName);
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char b = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = b;
+            System.out.println(b);
+        }
+
+        int[][] gameField = new int[3][3];
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                gameField[i][j] = 0;
+                if (gameField[i] == gameField[j]) {
+                    gameField[i][j] = 1;
+                } else {
+                    gameField[i][j] = 0;
+                }
+            }
+        }
+        for (int[] row : gameField) {
+            for (int column : row) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+}
+
